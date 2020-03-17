1a1cb16ebe3975ea08d2d96067c46057de96ffb6
vincentktsui <vincentktsui@gmail.com>
2020-03-17 15:31:07 -0700

    Revert "Merge pull request #3 from dannythanhphan/lobbies"
    This reverts commit be73dd48db3fe213f9cb4d61effeb706d70cbd5a, reversing
    changes made to a8d42edfc61c2e8caf064a2e7f63bdcb851c1be0.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 const db = require("./config/keys").mongoURI;
 
 const users = require("./routes/api/users");
-const lobbies = require("./routes/api/lobbies");
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
@@ -27,7 +26,6 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 
 app.use("/api/users", users);
-app.use("/api/lobbies", lobbies);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- models/User.js
+++ models/User.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     password: {
         type: String,
