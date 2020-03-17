389aa2b52498ed28e12d5795aa5beda81e4282d7
vincentktsui <vincentktsui@gmail.com>
2020-03-17 10:03:49 -0700

    Uncomment one line

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
-// const User = require("../models/User");
+const User = require("../models/User");
 
 const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
