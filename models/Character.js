78007f8a6678f2c30a61be338ba7218252639fbc
nicholepatrisse <nichole.patrisse@gmail.com>
2020-03-17 10:24:54 -0700

    Add Character Schema

---
+++ models/Character.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CharacterSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    level: {
+        type: Number,
+        default: 1,
+    },
+    currentHP: {
+        type: Number,
+        default: 100,
+    },
+    totalHP: {
+        type: Number,
+        default: 100,
+    },
+    meleeAttack: {
+        type: Number,
+        default: 50,
+    },
+    meleeSpeed: {
+        type: Number,
+        default: 1,
+    },
+    rangedAttack: {
+        type: Number,
+        default: 25,
+    },
+    rangedSpeed: {
+        type: Number,
+        default: 2,
+    },
+    movementSpeed: {
+        type: Number,
+        default: 1,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    // add lobby and items later
+});
+
+module.exports = Character = mongoose.model('characters', CharacterSchema);
