2a327551c9135133fa243e0149c7142b12c14c1b
vincentktsui <vincentktsui@gmail.com>
2020-03-17 15:28:29 -0700

    Temp save

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import charactersReducer from './characters_reducer';
+import lobbiesReducer from './lobbies_reducer';
 
 export default combineReducers({
     characters: charactersReducer,
+    lobbies: lobbiesReducer
 });
