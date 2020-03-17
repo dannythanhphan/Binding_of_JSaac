f38f149039c9ec318bd2102f61b97d9a9c15227e
vincentktsui <vincentktsui@gmail.com>
2020-03-16 22:34:19 -0700

    Setup user auth

---
+++ frontend/src/reducers/session_reducer.js
@@ -0,0 +1,27 @@
+import { RECEIVE_USER_LOGOUT,
+         RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+};
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: !!action.currentUser,
+                user: action.currentUser
+            };
+        case RECEIVE_USER_LOGOUT:
+            return {
+                isAuthenticated: false,
+                user: undefined
+            };
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
