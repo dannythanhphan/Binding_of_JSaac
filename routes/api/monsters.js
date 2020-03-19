2c2c2f9c641ce3c4da3e4cce8458285488fd9a1b
vincentktsui <vincentktsui@gmail.com>
2020-03-19 15:11:37 -0700

    Add monster death logic

--- routes/api/monsters.js
+++ routes/api/monsters.js
@@ -5,21 +5,21 @@ const ObjectId = require('mongodb').ObjectId;
 
 router.delete("/death/:id", (req, res) => {
     Lobby.update({
-        "dungeon.floors.rooms.monsters._monsterid": new ObjectId(req.params.id)}, 
+        "dungeon.floors.rooms.monsters._id": new ObjectId(req.params.id)}, 
         {$pull : {
             "dungeon.floors.$[].rooms.$[].monsters": 
-            { "_monsterid": new ObjectId(req.params.id)} } } )
+            { "_id": new ObjectId(req.params.id)} } } )
     .then(() => res.json({ dedMonster: "Monster has died." }))
     .catch(err => {res.status(404), json("Monster deletion failed")})
 });
 
-// router.get("/:id", (req, res) => {
-//     Dungeon.find({
-//         "floors.rooms.monsters._monsterid": new ObjectId(req.params.id)})
-//     .then((monster) => res.json(monster))
-//     .catch(err => {
-//         return res.status(400).json("Error getting monster");
-//     })
-// });
+router.get("/:id", (req, res) => {
+    Lobby.find({
+        "dungeon.floors.rooms.monsters._id": new ObjectId(req.params.id)})
+    .then((monster) => res.json(monster))
+    .catch(err => {
+        return res.status(400).json("Error getting monster");
+    })
+});
 
 module.exports = router;
