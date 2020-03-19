98539e524fd0e7be20f3a961a54fad13317ed45b
vincentktsui <vincentktsui@gmail.com>
2020-03-19 16:10:19 -0700

    Add imports to entities reducer

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -3,6 +3,10 @@ import { combineReducers } from 'redux';
 import charactersReducer from './characters_reducer';
 import lobbyReducer from './lobby_reducer';
 import usersReducer from './users_reducer';
+import floorsReducer from "./floors_reducer";
+import roomsReducer from "./rooms_reducer";
+import monstersReducer from "./monsters_reducer";
+import trapsReducer from "./traps_reducer";
 
 export default combineReducers({
     users: usersReducer,
