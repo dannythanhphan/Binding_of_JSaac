b84b259f2cdcc0c68d1557392b479cfad76248a8
nicholepatrisse <nichole.patrisse@gmail.com>
2020-03-19 19:55:57 -0700

    Basic modal setup

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -65,7 +65,7 @@ class MainPage extends React.Component {
                             <Link to="/main/create" className="redirect-buttons">
                                 New Character
                             </Link>
-                            <Link to="main/lobby" className="redirect-buttons">
+                            <Link to="/main/lobby" className="redirect-buttons">
                                 Start Game
                             </Link>
                         </div>
@@ -74,7 +74,7 @@ class MainPage extends React.Component {
                     {displayInstructions}
                     <ProtectedRoute path="/main/:characterId" component={CharacterSelectedContainer} />
                     <ProtectedRoute path="/main/create" component={CreateCharacterContainer}/>
-                    <ProtectedRoute path="main/lobby" component={LobbyContainer} />
+                    <ProtectedRoute path="/main/lobby" component={LobbyContainer} />
                 </div>
                 <Link to='/main'>
                     <img className="main-logo-image" src={logo} alt="logo" />
