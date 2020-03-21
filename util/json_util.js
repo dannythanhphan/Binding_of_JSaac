f150c0b70f3bfcaad25f0fd6a790485f0b66d2b7
Tony Ye <tonyyye@gmail.com>
2020-03-21 03:05:13 -0700

    Add locations to json_util

--- util/json_util.js
+++ util/json_util.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 
 const buildLobbyJson = (lobby, res) => {
-    const payload = { users: {}, characters: {}, lobby: {}, dungeon: {}, floors: {}, rooms: {}, monsters: {}, traps: {}};
+    const payload = { users: {}, characters: {}, lobby: {}, dungeon: {}, floors: {}, exits: {}, rooms: {}, monsters: {}, traps: {}};
     payload.lobby = {
         id: lobby.id,
         lobbykey: lobby.lobbykey,
@@ -16,7 +16,12 @@ const buildLobbyJson = (lobby, res) => {
             id: floor.id,
             dungeonId: lobby.dungeon.id,
             level: floor.level
-        } 
+        }
+        payload.exits[floor.exit.id] = {
+            location: floor.exit.location,
+            xPos: floor.exit.xPos,
+            yPos: floor.exit.yPos
+        }
         floor.rooms.forEach(room => {
             payload.rooms[room.id] = {
                 id: room.id,
@@ -58,6 +63,20 @@ const buildLobbyJson = (lobby, res) => {
             })
         })
     });
+    for (let i = 0; i < lobby.locations.length; i++) {
+        if ((lobby.player1 && lobby.player1 === lobby.locations[i].character) ||
+            (lobby.player2 && lobby.player2 === lobby.locations[i].character))
+        {
+            payload.locations[lobby.locations[i].character] = {
+                character: lobby.locations[i].character,
+                floor: lobby.locations[i].floor,
+                room: lobby.locations[i].room,
+                xPos: lobby.locations[i].xPos,
+                yPos: lobby.locations[i].yPos
+            }
+        } 
+    }
+
     User.find({ "characters._id": { $in: [lobby.player1, lobby.player2] } })
     .then(users => {
         users = users.filter(user => user.characters.length > 0);
@@ -69,6 +88,7 @@ const buildLobbyJson = (lobby, res) => {
             user.characters.forEach(character => {
                 payload.characters[character.id] = character
             });
+            
         })
         return res.json(payload)
     })
