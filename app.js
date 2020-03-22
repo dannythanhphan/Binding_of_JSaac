6b601991742d8a00fb7776120eda9bd8632845ed
Tony Ye <tonyyye@gmail.com>
2020-03-21 19:53:08 -0700

    Fix app.js path for production

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"));
+    app.use(express.static("frontend/public"));
     app.get("/", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
     });
 }
 
