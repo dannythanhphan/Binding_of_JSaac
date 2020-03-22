019c906822968bdedb987283f6ab44837d666ba7
Tony Ye <tonyyye@gmail.com>
2020-03-21 20:38:11 -0700

    Make modifications to files for heroku deployment

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+    app.get("/", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    });
+}
 
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const users = require("./routes/api/users");
 const lobbies = require("./routes/api/lobbies");
 const characters = require("./routes/api/characters");
 const monsters = require("./routes/api/monsters");
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require("path");
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/src"));
-    app.get("/", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
-    });
-}
+
 
 mongoose
     .connect(db, { useNewUrlParser: true })
