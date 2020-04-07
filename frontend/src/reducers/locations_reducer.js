0b11fe0c699fe1d86cb0d8f63fd176eceecef99e
vincentktsui <vincentktsui@gmail.com>
2020-04-07 16:32:31 -0700

    Merge

--- frontend/src/reducers/locations_reducer.js
+++ frontend/src/reducers/locations_reducer.js
@@ -9,12 +9,6 @@ const locationsReducer = (state = {}, action) => {
         case RECEIVE_LOBBY:
             return action.payload.locations;
 
-<<<<<<< HEAD
-        case UPDATE_LOCATION:
-            return action.payload.locations;
-
-=======
->>>>>>> 8219f2b2998cb0c794d8e51daa22e3c758524b20
         case REMOVE_LOBBY:
             return {};
 
